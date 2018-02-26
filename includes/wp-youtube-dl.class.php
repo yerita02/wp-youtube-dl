894b6813b3a68b9d937af24c0619f317bbb22862
Yeraldine Martínez <yerald02@hotmail.com>
2018-02-26 17:22:52 -0500

    Add files via upload
    Estrutura inicial plugin

---
+++ includes/wp-youtube-dl.class.php
@@ -0,0 +1,61 @@
+<?php 
+	
+if ( !class_exists( 'WP_YouTube_dl' ) ) {
+	class WP_YouTube_dl extends WP_Widget {
+		/**
+		 * Sets up the widgets name etc
+		 */
+		public function __construct() {
+			$widget_ops = array( 
+				'classname' => 'wp_youtube_dl',
+				'description' => 'Este plugin proveera las funcionalidades para hacer busquedas de videos desde un input tipo autocompletar. Los videos son obtenidos por medios de la API de YouTube',
+			);
+			parent::__construct( 'wp_youtube_dl', 'Buscador de Videos', $widget_ops );
+		}
+		
+
+		/**
+		 * Outputs the content of the widget
+		 *
+		 * @param array $args
+		 * @param array $instance
+		 */
+		public function widget( $args, $instance ) {
+			// outputs the content of the widget
+		}
+
+		/**
+		 * Outputs the options form on admin
+		 *
+		 * @param array $instance The widget options
+		 */
+		public function form( $instance ) {
+			// outputs the options form on admin
+		}
+
+		/**
+		 * Processing widget options on save
+		 *
+		 * @param array $new_instance The new options
+		 * @param array $old_instance The previous options
+		 *
+		 * @return array
+		 */
+		public function update( $new_instance, $old_instance ) {
+			// processes widget options to be saved
+		}
+
+		/**
+		 * Sets the active value to the option 'wp_youtube_dl-active'
+		 */
+		public static function init() {
+            add_option('wp_youtube_dl-active', 'yes');
+        }
+
+        public static function remove() {
+            delete_option('wp_youtube_dl-active');
+        }
+        
+	}
+}
+
---
+++ wp-youtube-dl.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* Plugin Name: Buscador de Videos
+* Plugin URI: 
+* Description: Este plugin proveera las funcionalidades para hacer busquedas de videos desde un input tipo autocompletar. Los videos son obtenidos por medios de la API de YouTube. 
+* Version: 1.0.0
+* Author: Yeraldine Martinez
+* Author URI: https://www.linkedin.com/in/yerita02
+* License: GPL2
+*/
+
+require_once('includes/wp-youtube-dl.class.php');
+
+//if ( is_admin() ) {
+	function add_admin_page(){
+    	add_menu_page(
+	        __( 'Buscador YouTube', 'textdomain' ),
+	        'Buscador YouTube',
+	        'manage_options',
+	        'wp-youtube-dl',
+	        'wp_youtube_dl_admin_page',
+	        'dashicons-video-alt3',
+	        6
+		);
+    }
+
+    function wp_youtube_dl_admin_page(){
+    	include('admin/wp-youtube-dl-admin.php'); 	
+    }
+
+	add_action( 'admin_init', 'add_admin_page' ); 
+//}
+
+register_activation_hook( __FILE__, 'wp_youtube_dl_init');
+function wp_youtube_dl_init(){	
+	WP_YouTube_dl::init();
+}
+
+register_deactivation_hook(__FILE__, 'wp_youtube_dl_remove' );
+function wp_youtube_dl_remove(){
+	WP_YouTube_dl::remove();
+}
